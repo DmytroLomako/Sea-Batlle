4c5ef0be6a9900e97c5cb626cab88337471ae1b3
DmytroLomako <you@example.com>
2024-06-26 18:07:41 +0300

    1-deck ship test

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 from turtle import *
 from random import *
 
-tracer(0,0)
+tracer(0, 0)
 speed(0)
 hideturtle()
 
 list = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
+list1 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,]
 
 def draw_figure(size, count):
     for i in range(count):
@@ -23,9 +34,7 @@ def draw_field(x, y):
         for j in range(10):
             draw_figure(30, 4)
             forward(30)
-            
-
-            
+                        
 def text():
     for i in range(9):
         penup()
@@ -63,16 +72,133 @@ def text():
         goto((-340 + i * 30), 183)
         write(list[index], font=("Arial", 13, "bold"))
         index += 1
-    
 
+draw = False
+text1 = Turtle()
+text1.hideturtle()
+text1.speed(0)
+text1.penup()
+text1.goto(-300, 230)
+text1.write('Arrange 1-deck ships', font=("Arial", 16, "bold"))
 
 def click(x, y):
-    print(x, y)
-    
+    global draw, text1
+    if draw == False:
+        if x >= -350 and x <= -320:
+            x = 0
+        elif x > -320 and x <= -290:
+            x = 1
+        elif x > -290 and x <= -260:
+            x = 2
+        elif x > -260 and x <= -230:
+            x = 3
+        elif x > -230 and x <= -200:
+            x = 4
+        elif x > -200 and x <= -170:
+            x = 5
+        elif x > -170 and x <= -140:
+            x = 6
+        elif x > -140 and x <= -110:
+            x = 7
+        elif x > -110 and x <= -80:
+            x = 8
+        elif x > -80 and x <= -50:
+            x = 9
+        else:
+            x = None
+        if y <= 180 and y >= 150:
+            y = 0
+        elif y < 150 and y >= 120:
+            y = 1
+        elif y < 120 and y >= 90:
+            y = 2
+        elif y < 90 and y >= 60:
+            y = 3
+        elif y < 60 and y >= 30:
+            y = 4
+        elif y < 30 and y >= 0:
+            y = 5
+        elif y < 0 and y >= -30:
+            y = 6
+        elif y < -30 and y >= -60:
+            y = 7
+        elif y < -60 and y >= -90:
+            y = 8
+        elif y < -90 and y >= -120:
+            y = 9
+        else:
+            y = None
+        if x!= None and y!= None:
+            index = x + y * 10
+            if list1[index] == 0:
+                draw = True
+                list1[index] = 1
+                list1[index + 1] = 1
+                list1[index - 1] = 1
+                list1[index + 10] = 1
+                list1[index - 10] = 1
+                list1[index + 11] = 1
+                list1[index - 11] = 1
+                list1[index + 9] = 1
+                list1[index - 9] = 1
+                for i in range(4):
+                    penup()
+                    goto((x * 30 - 320), (y * -30 + 150))
+                    pendown()
+                    draw_figure(30, 3)
+                    draw = False               
+                    
+# def boat1():
+#     for i in range(4):
+#         penup()
+#         seth(90)
+#         goto((-100 + i * 60), -200)
+#         pendown()
+#         draw_figure(25, 3)
+        
+# def boat2():
+#     for i in range(3):
+#         penup()
+#         seth(90)
+#         goto((-90 + i * 90), -250)
+#         pendown()
+#         draw_figure(25, 4)
+#         goto((-115 + i * 90), -250)
+#         draw_figure(25, 3)
+        
+# def boat3():
+#     for i in range(2):
+#         penup()
+#         seth(90)
+#         goto((-50 + i * 120), -300)
+#         pendown()
+#         draw_figure(25, 4)
+#         goto((-75 + i * 120), -300)
+#         draw_figure(25, 4)
+#         goto((-100 + i * 120), -300)
+#         draw_figure(25, 3)
+        
+# def boat4():
+    penup()
+    seth(90)
+    goto((-125 + 1 * 150), -350)
+    pendown()
+    draw_figure(25, 4)
+    goto((-150 + 1 * 150), -350)
+    draw_figure(25, 4)
+    goto((-175 + 1 * 150), -350)
+    draw_figure(25, 4)
+    goto((-200 + 1 * 150), -350)
+    draw_figure(25, 3)
+
 onscreenclick(click)
             
 draw_field(-350, 150)
 draw_field(50, 150)
 text()
+# boat1()
+# boat2()
+# boat3()
+# boat4()
 update()
 mainloop()
