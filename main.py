4bf89cd5e78b62198f3993a91a3cc465ce4880aa
DmytroLomako <you@example.com>
2024-06-29 15:59:19 +0300

    1-deck ship test 3

--- main.py
+++ main.py
@@ -6,17 +6,19 @@ speed(0)
 hideturtle()
 
 list_letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
-list1 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-list2 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-list3 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-list4 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-list5 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-list6 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-list7 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-list8 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-list9 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-list10 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-
+list1 = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+]
 def draw_figure(size, count):
     for i in range(count):
         forward(size)
@@ -85,66 +87,85 @@ def click(x, y):
     if draw == False:
         if x >= -350 and x <= -320:
             x = 0
-            list = list1
+            index_x = 0 
         elif x > -320 and x <= -290:
             x = 1
-            list = list2
+            index_x = 1
         elif x > -290 and x <= -260:
             x = 2
-            list = list3
+            index_x = 2
         elif x > -260 and x <= -230:
             x = 3
-            list = list4
+            index_x = 3
         elif x > -230 and x <= -200:
             x = 4
-            list = list5
+            index_x = 4
         elif x > -200 and x <= -170:
             x = 5
-            list = list6
+            index_x = 5
         elif x > -170 and x <= -140:
             x = 6
-            list = list7
+            index_x = 6
         elif x > -140 and x <= -110:
             x = 7
-            list = list8
+            index_x = 7
         elif x > -110 and x <= -80:
             x = 8
-            list = list9
+            index_x = 8
         elif x > -80 and x <= -50:
             x = 9
-            list = list10
+            index_x = 9
         else:
             x = None
         if y <= 180 and y >= 150:
             y = 0
+            index_y = 0
         elif y < 150 and y >= 120:
             y = 1
+            index_y = 1
         elif y < 120 and y >= 90:
             y = 2
+            index_y = 2
         elif y < 90 and y >= 60:
             y = 3
+            index_y = 3
         elif y < 60 and y >= 30:
             y = 4
+            index_y = 4
         elif y < 30 and y >= 0:
             y = 5
+            index_y = 5
         elif y < 0 and y >= -30:
             y = 6
+            index_y = 6
         elif y < -30 and y >= -60:
             y = 7
+            index_y = 7
         elif y < -60 and y >= -90:
             y = 8
+            index_y = 8
         elif y < -90 and y >= -120:
             y = 9
+            index_y = 9
         else:
             y = None
         if x!= None and y!= None:
-            index = y
-            if list[index] == 0:
+            if list1[index_x][index_y] == 0:
                 draw = True
-                list[index] = 1
+                list1[index_x][index_y] = 1
+                list1[index_x + 1][index_y] = 1
+                list1[index_x - 1][index_y] = 1
+                list1[index_x][index_y + 1] = 1
+                list1[index_x + 1][index_y + 1] = 1
+                list1[index_x - 1][index_y + 1] = 1  
+                list1[index_x][index_y - 1] = 1
+                list1[index_x + 1][index_y - 1] = 1
+                list1[index_x - 1][index_y - 1] = 1 
+                
                 for i in range(4):
                     penup()
-                    goto((x * 30 - 350), (y * -30 + 150))
+                    goto((x * 30 - 350), (y * -30 + 180))
+                    seth(270)
                     pendown()
                     draw_figure(30, 3)
                     draw = False               
